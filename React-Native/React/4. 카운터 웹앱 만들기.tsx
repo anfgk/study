e6e6f3d0da8d180cdbb7f5e821d1ecaa40df46ba
anfgk <gloria020510@gmail.com>
2025-06-23 17:13:56 +0900

    카운터 웹앱 만들기

---
+++ React-Native/React/4. 카운터 웹앱 만들기.tsx
@@ -0,0 +1,18 @@
+// import React from "react";
+
+// function App() {
+// const [variable, setVariable] = useState(0);
+// function editVariable() {
+// setVariable((pre) => pre +1);
+// setState((이전값) => ...)를 통해 이전값을 읽을 수 있고,
+// 이 값을 넘겨서 사용할 수도 있습니다. 기본적 형태는 위와 같은 ((이전값) => ...) 입니다.
+// console.log(variable);
+// }
+
+//     return (
+//         <div className="App">
+//             <button onClick={() => editVariable()}>count + 1</button>
+//             <h1>{variable}</h1>
+//         </div>
+
+// export default App;
